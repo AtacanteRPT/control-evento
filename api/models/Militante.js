cfab73d7a2bc9bd2b4d502ce4b7c13df5b64014a
AtacanteRPT <rpaucara13@umsa.bo>
2019-03-11 17:35:28 -0400

    version con genero

--- api/models/Militante.js
+++ api/models/Militante.js
@@ -38,6 +38,9 @@ module.exports = {
         hora: {
             type: 'string'
         },
+        sexo: {
+            type: 'string'
+        },
         // usuario:{
         //   collection:'usuario',
         //   via:'idMilitante'
